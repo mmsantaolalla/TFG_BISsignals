ed9048660866b8be72a8e9c8116ad153840eb5c3
mmsantaolalla <61518634+mmsantaolalla@users.noreply.github.com>
2022-11-06 18:59:30 +0100

    Update model.R
    Translating to english some useful comments for the understanding of the code

--- model.R
+++ model.R
@@ -2,25 +2,25 @@ library(tidymodels)
 library(doParallel)
 library(themis)
 
-set.seed(123) # evitamos que cada vez que corremos al ser resultados aleatorios distintos
+set.seed(123) # We avoid that every time we run the results vary (because they are random).
 
 data = read.csv("resultados/final_data.csv")
 data$attack = factor(data$attack)
 
- # seleccionar el archivo y la columna de la que se hace referencia
+ #divide between training and test files (cross validation)
 files <-  unique(data$file)
-test_files <- files[(length(files) - 2):length(files)]
-train_files <- files[1:(length(files) - 3)]
+test_files <- files[(length(files) - 2):length(files)] # only the last 3 files are used for testing the prediction
+train_files <- files[1:(length(files) - 3)] # the rest are used in the model
 trees_train <- filter(data, file %in% train_files)
 trees_test <- filter(data, file %in% test_files)
 
 trees_train$file = NULL
 trees_test$file = NULL
 
-tree_rec <- recipe(attack ~ ., data = trees_train) %>% # hacer el estudio respecto a la columna attack (el resto respecto a la de referencia)
-  step_upsample() # balanceamos datoss
+tree_rec <- recipe(attack ~ ., data = trees_train) %>%
+  step_upsample() # data balancing
 
-tune_spec <- rand_forest( # creamos la funcion random forest
+tune_spec <- rand_forest( # creating the random forest function
   mtry = tune(),
   trees = tune(),
   min_n = tune()
@@ -28,23 +28,23 @@ tune_spec <- rand_forest( # creamos la funcion random forest
   set_mode("classification") %>%
   set_engine("ranger")
 
-tune_wf <- workflow() %>% # combinammos columna principal(attack) con el resto de columnas seleccionadas anteriormente
+tune_wf <- workflow() %>% 
   add_recipe(tree_rec) %>%
   add_model(tune_spec)
 
-trees_folds <- vfold_cv(trees_train) # llevamos a cabo la cross validation del estudio
+trees_folds <- vfold_cv(trees_train) # Cross validation of the model data (10 folds)
 
 doParallel::registerDoParallel() # (buffer)
 
 set.seed(345) 
-tune_res <- tune_grid( # generamos los foldings del arbol( seleccionamos un resultado de 20 muestras )
+tune_res <- tune_grid( # tuning the model( especifying the number of trees and folds)
   tune_wf,
   resamples = trees_folds,
-  grid = 20 # numero de muestras optimo por tiempo y accuracy  
+  grid = 20 # optimal number of trees per time and accuracy  
 )
 
 
-best_auc <- select_best(tune_res, "roc_auc") # seleccionamos de los 20 modelos el mejor
+best_auc <- select_best(tune_res, "roc_auc") # from the 20 model results, the best one is selected
 final_wf <- finalize_workflow(
   tune_wf,
   best_auc
@@ -56,10 +56,10 @@ final_res <- final_wf %>% fit(trees_train)
 preds_df <- final_res %>% predict(trees_test) %>% bind_cols("truth"=trees_test$attack)
 
 print(
-  conf_mat(preds_df, truth = truth, estimate = .pred_class)
+  conf_mat(preds_df, truth = truth, estimate = .pred_class) #confusion matrix is generated
 )
 
 
-# REFERENCIAS: 
+# REFERENCES: 
 # https://juliasilge.com/blog/sf-trees-random-tuning/
 # https://themis.tidymodels.org/reference/step_upsample.html
